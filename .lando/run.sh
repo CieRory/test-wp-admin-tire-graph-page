7e4346f391af1a85b35799db5344f8119c96c816
Rory Heaney <rheaney@ciedigital.com>
2025-10-06 18:13:05 -0700

    Base lando setup for a local dev environment

---
+++ .lando/run.sh
@@ -0,0 +1,11 @@
+if [ ! -d "./wp-admin" ]; then
+	wp core download
+fi
+
+# if ! $(wp core is-installed); then
+# 	wp core install --url="http://wp-starter.local" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="admin@wp-starter.local" --skip-email
+# fi
+
+if ! $(wp core is-installed); then
+	wp core install --url="http://$LANDO_APP_NAME.lndo.site" --title="WP Starter" --admin_user="admin" --admin_password="password" --admin_email="rheaney@ciedigital.com" --skip-email
+fi
---
+++ wp-content/themes/sage-wordpress-scripts/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Sage includes
+ *
+ * The $sage_includes array determines the code library included in your theme.
+ * Add or remove files to the array as needed. Supports child theme overrides.
+ *
+ * Please note that missing files will produce a fatal error.
+ *
+ * @link https://github.com/roots/sage/pull/1042
+ */
+$sage_includes = [
+	'lib/bs-5-nav-walker.php', // BS 5 nav walker
+	'lib/assets.php',    // Scripts and stylesheets
+	'lib/extras.php',    // Custom functions
+	'lib/setup.php',     // Theme setup
+	'lib/titles.php',    // Page titles
+	'lib/wrapper.php',   // Theme wrapper class
+	'lib/customizer.php', // Theme customizer
+];
+
+foreach ($sage_includes as $file) {
+	if (!$filepath = locate_template($file)) {
+		trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+	}
+
+	require_once $filepath;
+}
+unset($file, $filepath);
+
+// Uncomment the following lines to add excerpts support for 'service' post type
+// add_action('init', 'fancy_squares_add_excerpts_to_pages');
+// function fancy_squares_add_excerpts_to_pages()
+// {
+// 	add_post_type_support('service', 'excerpt');
+// }
